97971e85699cda1be5becb6f2fd23028231708a1
Administrator <Administrator@ASUS>
2020-06-02 16:24:22 +0800

    "2020.06.02,搭建框架第一个接口"

---
+++ Day48_springboot/src/main/java/com/xxl/SpringBootDemo/Day48SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.xxl.SpringBootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day48SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day48SpringbootApplication.class, args);
+	}
+
+}
