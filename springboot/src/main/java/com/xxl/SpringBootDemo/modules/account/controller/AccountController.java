1ac8e65de84d22eda734343428b9a6cc0dc94982
Administrator <Administrator@ASUS>
2020-06-16 16:36:53 +0800

    "2020-6-10 拆解前端模板"

--- springboot/src/main/java/com/xxl/SpringBootDemo/modules/account/controller/AccountController.java
+++ springboot/src/main/java/com/xxl/SpringBootDemo/modules/account/controller/AccountController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/account")
+@RequestMapping("/account") 
 public class AccountController {	
 	
 	//登录页
